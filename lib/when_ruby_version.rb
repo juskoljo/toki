6472fdd230b18ca4d785be9dc6dfb6f5899f05a6
Jussi Koljonen <jussi.koljonen@gmail.com>
2013-10-27 02:16:07 +0300

    Renamed when_ruby_platform to when_platform; now uses RbConfig::CONFIG['host_os'] instead of RUBY_PLATFORM Added support to JRuby Added support to MacRuby Added support to RBX

--- lib/when_ruby_version.rb
+++ lib/when_ruby_version.rb
@@ -1,3 +1,4 @@
+require 'rbconfig'
 require File.expand_path('../when_ruby_version/version', __FILE__)
 
 module WhenRubyVersion
@@ -8,21 +9,69 @@ module WhenRubyVersion
       case key
       when :version
         when_ruby_version(value)
+      when :jruby_version
+        when_jruby_version(value)
+      when :macruby_version
+        when_macruby_version(value)
+      when :rbx_version
+        when_rbx_version(value)
       when :engine
         when_ruby_engine(value)
       when :patchlevel
         when_ruby_patchlevel(value)
       when :platform
-        when_ruby_platform(value)
+        when_platform(value)
       else
         raise ArgumentError, "unsupported key #{key} for when_ruby options"
       end
     end
   end
 
+  # JRUBY_VERSION
+  def when_jruby_version(pattern, &block)
+    if defined?(JRUBY_VERSION)
+      if pattern_matches_with?(JRUBY_VERSION, pattern)
+        apply_behaviour(&block)
+      else
+        false
+      end
+    else
+      # JRUBY_VERSION not defined
+      false
+    end
+  end
+
+  # MACRUBY_VERSION
+  def when_macruby_version(pattern, &block)
+    if defined?(MACRUBY_VERSION)
+      if pattern_matches_with?(MACRUBY_VERSION, pattern)
+        apply_behaviour(&block)
+      else
+        false
+      end
+    else
+      # MACRUBY_VERSION not defined
+      false
+    end
+  end
+
+  # Rubinius::VERSION
+  def when_rbx_version(pattern, &block)
+    if defined?(Rubinius)
+      if pattern_matches_with?(Rubinius::VERSION, pattern)
+        apply_behaviour(&block)
+      else
+        false
+      end
+    else
+      # Rubinius not defined
+      false
+    end
+  end
+
   # RUBY_VERSION
   def when_ruby_version(pattern, &block)
-    if pattern_matches_with?("#{RUBY_VERSION}p#{RUBY_PATCHLEVEL}", pattern)
+    if pattern_matches_with?(RUBY_VERSION, pattern)
       apply_behaviour(&block)
     else
       false
@@ -50,15 +99,39 @@ module WhenRubyVersion
   end
 
   # RUBY_PLATFORM
-  def when_ruby_platform(pattern, &block)
-    if pattern_matches_with?(RUBY_PLATFORM, pattern)
-      apply_behaviour(&block)
+  def when_platform(pattern, &block)
+    case pattern
+    when Symbol
+      if pattern == detect_os
+        apply_behaviour(&block)
+      else
+        false
+      end
     else
-      false
+      if pattern_matches_with?(RbConfig::CONFIG['host_os'], pattern)
+        apply_behaviour(&block)
+      else
+        false
+      end
     end
   end
 
   private
+
+  def detect_os    
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|windows|win32|cygwin|bccwin|wince|emc/i
+      :windows
+    when /darwin|mac os/i
+      :osx
+    when /linux/i
+      :linux
+    when /solaris|sunos|bsd/i
+      :unix
+    else
+      :unknown
+    end
+  end
 
   def pattern_matches_with?(constant, pattern)
     constant.to_s =~ case pattern
@@ -77,14 +150,4 @@ module WhenRubyVersion
     true
   end
 
-end
-
-# # apply with WithRubyVersion to Module
-# class Module
-#   include WithRubyVersion
-# end
-
-# # apply with WithRubyVersion to Class
-# class Class
-#   include WithRubyVersion
-# end
+end # WhenRubyVersion
